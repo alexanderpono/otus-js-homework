e996783c758b305e301c0cc7d4d58cc42bd66318
Alex <ap@nikolasoft.ru>
2018-05-10 02:27:57 +0300

    lesson15 - vue-game - screen "game"

--- vue-game/src/components/Game.vue
+++ vue-game/src/components/Game.vue
@@ -13,12 +13,16 @@
 
         </div>
 
+        <div class="quizNumber">
+            Задача №{{quizNumber}}
+        </div>
+
         <div class="quiz">
             <span id="num1">{{firstOperand}}</span>
             {{mathOperation1}}
-            <input id="num2" type="text" v-model="answer1" />
+            <input id="num2" type="text" v-model="answer1" v-bind:class="num2Class"/>
             {{mathOperation2}}
-            <input id="num3" type="text" v-model="answer2" />
+            <input id="num3" type="text" v-model="answer2" v-bind:class="num3Class"/>
             <span id="question">
                 = {{supposedAnswer}}?
             </span>
@@ -58,11 +62,24 @@
             </div>
         </div>
 
+        <div id="lastAnswerStat" v-bind:class="lastAnswerStatClass">
+            <p><span class="parameter">Ваш ответ на задачу №{{quizNumber}}:</span> {{userLastAnswer}}</p>
+            <p><span class="parameter">Целевое значение:</span> {{lastSupposedAnswer}}</p>
+            <p><span class="parameter">Точность ответа, %:</span> {{lastAnswerAccuracy}}</p>
+        </div>
+
     </div>
 </template>
 
 <script>
 import AppMenu from './Menu.vue'
+import appState from '@/state'
+
+const INPUT1 = 'num2'
+const INPUT2 = 'num3'
+const ANSWER_VAR1 = 'answer1'
+const ANSWER_VAR2 = 'answer2'
+const NO_ANSWER_YET = -0.5
 
 export default {
     name: 'HelloWorld',
@@ -75,7 +92,16 @@ export default {
             firstOperand: 14,
             supposedAnswer: 84241,
             mathOperation1: '*',
-            mathOperation2: '+'
+            mathOperation2: '+',
+            currentInput: INPUT1,
+            quizNumber: 0,
+            quizesNumber: appState.getVal('quizesNumber'),
+            quizesAccuracy: appState.getVal('quizesAccuracy'),
+            userLastAnswer: NO_ANSWER_YET,
+            lastAnswerAccuracy: 0,
+            lastSupposedAnswer: 0,
+            thisSessionQuizNumber: 0,
+            thisSessionQuizSolved: 0
         }
     },
     methods: {
@@ -83,24 +109,164 @@ export default {
             this.$router.push('/settings/')
         },
         btNumber: function (param) {
-            console.log('btNumber() pressed! param=', param)
+            let targetAnswerVar = ANSWER_VAR1
+            if (this.currentInput !== INPUT1) {
+                targetAnswerVar = ANSWER_VAR2
+            }
+            this[targetAnswerVar] = '' + this[targetAnswerVar] + param
         },
         btLess: function (param) {
-            console.log('btLess() pressed!')
+            this.switchInput()
         },
         btMore: function (param) {
-            console.log('btMore() pressed!')
+            this.switchInput()
         },
         btQuestion: function (param) {
-            console.log('btQuestion() pressed!')
+            this.reQuiz()
         },
-        btEqual: function (param) {
-            console.log('btEqual() pressed!')
-        }
+        switchInput: function () {
+            if (this.currentInput === INPUT1) {
+                this.currentInput = INPUT2
+                this.answer2 = ''
+            } else {
+                this.currentInput = INPUT1
+                this.answer1 = ''
+            }
+        },
 
+        btEqual: function (param) {
+            let userAnswer = this.getUserAnswer()
+            let answerAccuracy = this.getAnswerAccuracy(userAnswer)
+            this.quizesNumber++
+            this.quizesAccuracy.push(answerAccuracy)
+            appState.setVal('quizesAccuracy', this.quizesAccuracy)
+
+            this.userLastAnswer = userAnswer
+            this.lastAnswerAccuracy = answerAccuracy
+            this.lastSupposedAnswer = this.supposedAnswer
+            this.thisSessionQuizNumber++
+            appState.setVal('lastTotal', this.thisSessionQuizNumber)
+
+            if (answerAccuracy !== 0) {
+                this.thisSessionQuizSolved++
+            }
+            appState.setVal('lastSolved', this.thisSessionQuizSolved)
+
+            this.nextQuiz()
+        },
+
+        op: function (mathOperationCode, operand1, operand2) {
+            switch (mathOperationCode) {
+            case '*':
+                return operand1 * operand2
+            case '+':
+                return operand1 + operand2
+            case '-':
+                return operand1 - operand2
+            case '/':
+                return operand1 / operand2
+            }
+            return 0
+        },
+
+        getUserAnswer: function () {
+            let answer1 = parseInt(this.answer1)
+            let answer2 = parseInt(this.answer2)
+            if (isNaN(answer1)) {
+                answer1 = 0
+            }
+            if (isNaN(answer2)) {
+                answer2 = 0
+            }
+            let userAnswer = this.op(this.mathOperation1, this.firstOperand, answer1)
+            userAnswer = this.op(this.mathOperation2, userAnswer, answer2)
+
+            return userAnswer
+        },
+
+        getAnswerAccuracy: function (userAnswer) {
+            let deviation = Math.abs(userAnswer - this.supposedAnswer) / this.supposedAnswer
+            let answerAccuracy = 100 - Math.round(deviation * 100)
+            if (answerAccuracy < 0) {
+                answerAccuracy = 0
+            }
+
+            return answerAccuracy
+        },
+
+        nextQuiz: function () {
+            this.quizNumber++
+            this.reQuiz()
+        },
+
+        reQuiz: function () {
+            this.firstOperand = Math.round(100 * Math.random())
+            this.supposedAnswer = Math.round(100000 * Math.random())
+            this.answer1 = ''
+            this.answer2 = ''
+
+            let ops = []
+            if (appState.getVal('sum')) {
+                ops.push('+')
+            }
+            if (appState.getVal('sub')) {
+                ops.push('-')
+            }
+            if (appState.getVal('mul')) {
+                ops.push('*')
+            }
+            if (appState.getVal('div')) {
+                ops.push('/')
+            }
+            let op1 = ops[Math.floor(ops.length * Math.random())]
+            let op2 = ops[Math.floor(ops.length * Math.random())]
+            if ((op2 === '*') || (op2 === '/')) {
+                let tmp = op1
+                op1 = op2
+                op2 = tmp
+            }
+
+            this.mathOperation1 = op1
+            this.mathOperation2 = op2
+        }
     },
     components: {
         AppMenu
+    },
+    computed: {
+        num2Class: function () {
+            let newClass = ''
+            if (this.currentInput === INPUT1) {
+                newClass = 'cur'
+            }
+            return newClass
+        },
+
+        num3Class: function () {
+            let newClass = ''
+            if (this.currentInput === INPUT2) {
+                newClass = 'cur'
+            }
+            return newClass
+        },
+
+        lastAnswerStatClass: function () {
+            let newClass = ''
+            if (this.userLastAnswer === NO_ANSWER_YET) {
+                newClass = 'hideme'
+            }
+            return newClass
+        }
+
+    },
+    created: function () {
+        this.quizNumber = this.quizesNumber
+
+        this.nextQuiz()
+    },
+    activated: function () {
+        this.thisSessionQuizNumber = 0
+        this.thisSessionQuizSolved = 0
     }
 
 }
@@ -201,6 +367,31 @@ export default {
     nav {
         text-align: center;
 
+    }
+
+    .cur {
+        background: green;
+        color: white;
+    }
+
+    .quizNumber {
+        text-align: center;
+    }
+
+    #lastAnswerStat {
+        margin-left: 100px;
+    }
+    #lastAnswerStat P {
+        margin: 0px;
+    }
+    #lastAnswerStat .parameter {
+        display: block;
+        width: 200px;
+        float: left;
+    }
+
+    .hideme {
+        display: none;
     }
 
 </style>
--- vue-game/src/state/index.js
+++ vue-game/src/state/index.js
@@ -8,10 +8,12 @@ export default {
         mul: false,
         div: false,
         pwr: false,
-        day: 23,
+        day: 1,
         lastSolved: 10,
         lastTotal: 25,
-        accuracy: 80
+        quizesAccuracy: [],
+        lastSessionQuizNumber: 0,
+        lastSessionQuizSolved: 0
     },
 
     setVal: function (key, val) {
@@ -23,6 +25,37 @@ export default {
         if ((typeof storageVal === 'undefined') || (storageVal === null)) {
             if (typeof (this.defaults[key]) !== 'undefined') {
                 return this.defaults[key]
+            }
+        }
+        if (storageVal === 'true') {
+            return true
+        }
+        if (storageVal === 'false') {
+            return false
+        }
+        if (key === 'quizesAccuracy') {
+            storageVal = storageVal.split(',')
+        }
+        if (key === 'quizesNumber') {
+            let quizesAccuracy = this.getVal('quizesAccuracy')
+            storageVal = quizesAccuracy.length
+        }
+
+        if (key === 'accuracy') {
+            let quizesAccuracy = this.getVal('quizesAccuracy')
+            let accuracySum = 0
+            for (let i = 0; i < quizesAccuracy.length; i++) {
+                let accuracy = parseInt(quizesAccuracy[i])
+                if (isNaN(accuracy)) {
+                    accuracy = 0
+                }
+                accuracySum += accuracy
+            }
+
+            if (quizesAccuracy.length > 0) {
+                storageVal = Math.round(accuracySum / quizesAccuracy.length)
+            } else {
+                storageVal = 0
             }
         }
         return storageVal
