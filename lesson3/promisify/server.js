5882213b8c10113686fb288d189bda7a7314df48
Alex <ap@nikolasoft.ru>
2018-05-30 01:21:30 +0300

    lesson3-promisify - updated version of the promisify() function 	now takes an only parameter

--- lesson3/promisify/server.js
+++ lesson3/promisify/server.js
@@ -1,55 +1,56 @@
 console.log('=== promisify ===')
 const fs = require('fs')
 
+function promisify(f) {
+    return function(param1, param2) {
+        let param2Undefined = (arguments.length === 1);
+        return new Promise(function(resolve, reject) {
+            if (param2Undefined) {
+                let defaultCallback = f;
+                defaultCallback(param1, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        resolve(data);
+                    }
+                });
+            }
+            else {
+                let singleCallback = f;
+                singleCallback(param1, param2, (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        resolve('');
+                    }
+                });
+            }
 
-function promisify(f, param1, param2) {
-    var param2Undefined = (arguments.length === 2);
-    var promise = new Promise(function(resolve, reject) {
-        if (param2Undefined) {
-            let defaultCallback = f;
-            defaultCallback(param1, (err, data) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(data);
-                };
-            });
-        }
-        else {
-            let singleCallback = f;
-            singleCallback(param1, param2, (err) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve('');
-                }
-            });
-        }
-    });
-
-    return promise;
+        });
+    };
 }
 
-
-promisify(fs.readFile, '/etc/passwd')
-    .then((data)=>console.log('promisify-fs.readFile(/etc/passwd) ' + data.toString()))
-    .catch((err)=>{console.log('promisify-fs.readFile(/etc/passwd) error...')})
+let readFilePromise = promisify(fs.readFile);
+readFilePromise('/etc/passwd')
+    .then((data)=>console.log('readFilePromise(/etc/passwd) ' + data.toString()))
+    .catch((err)=>{console.log('readFilePromise(/etc/passwd) error...')})
 ;
 
-promisify(fs.readFile, './1.txt')
-    .then((data)=>console.log('promisify-fs.readFile(./1.txt) ' + data.toString()))
-    .catch((err)=>{console.log('promisify-fs.readFile(./1.txt) error...')})
+readFilePromise('./1.txt')
+    .then((data)=>console.log('readFilePromise(./1.txt) ' + data.toString()))
+    .catch((err)=>{console.log('readFilePromise(./1.txt) error...')})
 ;
 
-
-promisify(fs.access, '/etc/passwd', fs.constants.R_OK)
-    .then((data)=>console.log('promisify-fs.access(/etc/passwd) read access' + data.toString()))
-    .catch((err)=>{console.log('promisify-fs.access(/etc/passwd) no access!')})
+let accessPromise = promisify(fs.access);
+accessPromise('/etc/passwd', fs.constants.R_OK)
+    .then((data)=>console.log('accessPromise(/etc/passwd) read access' + data.toString()))
+    .catch((err)=>{console.log('accessPromise(/etc/passwd) no access!')})
 ;
 
-promisify(fs.access, './1.txt', fs.constants.R_OK)
-    .then((data)=>console.log('promisify-fs.access(./1.txt) read access ' + data.toString()))
-    .catch((err)=>{console.log('promisify-fs.access(./1.txt) no access!')})
+accessPromise('./1.txt', fs.constants.R_OK)
+    .then((data)=>console.log('accessPromise(./1.txt) read access ' + data.toString()))
+    .catch((err)=>{console.log('accessPromise(./1.txt) no access!')})
 ;
+
